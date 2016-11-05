d909cd6a2e2a4981776829a78408926b7212a99b
Da <1591820729@qq.com>
2016-11-05 20:14:22 +0800

    Lab6 Service的使用实验 使用最简单的做法,给MainActivity直接访问Service中方法的权限,没有使用IPC机制

---
+++ app/src/main/java/com/example/da/lab6/MusicService.java
@@ -0,0 +1,68 @@
+package com.example.da.lab6;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.animation.RotateAnimation;
+
+import java.io.IOException;
+
+public class MusicService extends Service {
+    public  MediaPlayer mediaPlayer = new MediaPlayer();
+    public final IBinder binder = new MyBinder();
+    public class MyBinder extends Binder{
+        /*使用ontransact函数可以避免mediaplayer被其他类访问的情况,即可以将这些函数设置为private*/
+        @Override
+        protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            switch(code){
+                case 101:playOrPause();break;
+                case 102:stop();break;
+            }
+            return super.onTransact(code, data, reply, flags);
+        }
+
+        MusicService getService() {
+            return MusicService.this;
+        }
+
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return binder;
+    }
+    public MusicService() {
+        try {
+            mediaPlayer.setDataSource("/data/K.Will-Melt.mp3");
+            Log.i("getdata", "MusicService: ");
+            mediaPlayer.prepare();
+            mediaPlayer.setLooping(true);
+        } catch (IOException e) {
+            Log.d("Hint", "Can't not open the song ");
+            e.printStackTrace();
+        }
+    }
+    public void playOrPause(){
+        if (mediaPlayer.isPlaying()) mediaPlayer.pause();
+        else mediaPlayer.start();
+    }
+    public void stop(){
+        if (mediaPlayer != null){
+            mediaPlayer.stop();
+            //刚开始的写法为停止后直接prepare然后seekto(0),但是会出现点击停止按钮后依然后播放一小段才能够完全停止的状况,然后改为以下的方法
+            //停止后将mediaplayer重置后再重新加载音乐文件，这样就不会出现问题,具体原因未明,但是这种方法好像也不太好,待其他解决方法
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource("/data/K.Will-Melt.mp3");
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
